d68ef6f0028b204b0cdd9d6fc296ae7fbf15eae9
Ben Galde <bgalde.dev@gmail.com>
2021-03-31 23:38:25 -0700

    Added step 2

--- leaflet-step-1/static/js/config.js
+++ leaflet-step-1/static/js/config.js
@@ -1,2 +1,2 @@
-// API key
-const API_KEY = "YOUR API KEY";
+// API key I know I shouldn't put my key in here but needed it for it to show on the pages
+const API_KEY = "pk.eyJ1IjoiYmdhbGRlLWRldiIsImEiOiJja21oejh6ajEwY2doMnBsOHBvNGxiNzRqIn0.2LCXtkjW_DGcYVZistUVLA";
